afbb0e82b21b251e111e2552c7b81473471f5071
System administrator <CuteDoge>
2023-09-25 22:38:09 +0900

    leap scroll theme

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -28,6 +28,14 @@ local plugins = {
   },
 
   {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require("neoscroll").setup()
+    end,
+    lazy = false,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = overrides.treesitter,
   },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,6 +59,10 @@ local default_plugins = {
     'ggandor/leap.nvim',
     config = function()
       require("leap").add_default_mappings()
+      vim.keymap.set("n", "s", function() 
+        local current_window = vim.fn.win_getid()
+        require("leap").leap { target_windows = { current_window } }
+      end)
     end,
     dependencies = {
       'tpope/vim-repeat',
